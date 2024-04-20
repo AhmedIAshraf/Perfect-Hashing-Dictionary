bddfdae1b65ac668ed4bd7456a0c1776c7eab67f
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-04-20 15:10:35 +0200

    Add files via upload

--- Dictionary/src/HashTable.java
+++ Dictionary/src/HashTable.java
@@ -1,38 +1,54 @@
+import java.util.Random;
+
 public class HashTable <T> {
 
     protected T[] primaryTable;
+    protected
     protected int size=10;
-    protected double loadFactor;
     protected byte[][] hashFunction;
     protected int allocated=0;
-    protected int numberOfRehashing=0;
-    private HashTable<T> tableConstructed;
+    protected int counterOfRehashing=0;
+    Random rand=new Random();
 
-    protected HashTable(){};
-    public HashTable (int type) {
+    protected HashTable () {}
+
+    public HashTable<T> createTable (int type){
         this.primaryTable = (T[]) new Object[this.size*this.size];
         for (int i = 0; i < this.size*this.size ; i++) {this.primaryTable[i] = null;}
-        this.hashFunction = new byte[this.size][32];
-        //Randomize the hash function
+        generateHashFunction();
         this.allocated = 0;
-        this.numberOfRehashing=0;
-
-        if(type==1) tableConstructed= new LinearSpace<T>();
-        else if (type==2) tableConstructed= new QuadraticSpace<T>();
+        if(type==1) return new LinearSpace<T>();
+        else return new QuadraticSpace<T>();
     }
 
     protected void generateHashFunction(){
-
-    }
-
-    protected void reHashing(){
-        //don't forget the rehashing counter
+        this.hashFunction = new byte[this.size][32];
+        for (int i = 0; i < this.size ; i++) {
+            for (int j = 0; j < 32; j++) {
+                hashFunction[i][j] = (byte) rand.nextInt(2);
+            }
+        }
     }
 
     protected int getHashIndex(T value){
         int index=0;
-        //Do the mapping using the hash function
+        String bin = Integer.toBinaryString(value.hashCode());
+        byte word[] = new byte[32];
+        for (int i = 0; i < bin.length(); i++) { word[i] = (byte) (bin.indexOf(i) == '0'? 0 : 1); }
+        byte[] indexInBin = matrixMultiplication(this.hashFunction,word);
+        Integer.parseInt(new String(indexInBin),2);
         return index;
+    }
+
+    private byte[] matrixMultiplication(byte[][] a, byte[] b){
+        byte[] c = new byte[this.size];
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < 32; j++) {
+                c[i] += (a[i][j]*b[j])%2;
+            }
+            c[i] %= 2;
+        }
+        return c;
     }
 
 }
--- Dictionary/src/QuadraticSpace.java
+++ Dictionary/src/QuadraticSpace.java
@@ -1,20 +1,24 @@
-public class QuadraticSpace<T> extends HashTable{
+import java.util.ArrayList;
+
+public class QuadraticSpace<T> extends HashTable<T>{
 
     public boolean insert(T value){
         int index=getHashIndex(value);
         if (this.primaryTable[index]==null){
             this.primaryTable[index]=value;
             this.allocated++;
-            if ((double)this.allocated/(double)this.size >= 0.8){
-                System.out.println("Rehashing is Necessary as the Load Factor (n/m) Exceeded 0.8");
-                reHashing();
-            }
+//            if ((double)this.allocated/(double)this.size >= 0.8){
+//                System.out.println("Rehashing is Necessary as the Load Factor (n/m) Exceeded 0.8");
+//                reHashing(this.size*2);
+//            }
             return true;
         }else if (this.primaryTable[index].equals(value)){
             return false; //the element already exists
         }else{
             System.out.println("Rehashing is Necessary as There is a Collision While Inserting '"+value+"'.");
-            reHashing();
+            T[] add = (T[]) new Object[1];
+            add[0]=value;
+            reHashing(this.size,add);
             return true;
         }
     }
@@ -25,7 +29,7 @@ public class QuadraticSpace<T> extends HashTable{
             return false; //The element isn't in the table
         }else{
             this.allocated--;
-            this.primaryTable[index] = "";
+            this.primaryTable[index] = null;
             return true;
         }
     }
@@ -36,12 +40,75 @@ public class QuadraticSpace<T> extends HashTable{
     }
 
     public void batchInsert(T[] items){
+        int newLength=items.length+this.size;
+        reHashing(items.length+this.size,items);
 
     }
 
     public void batchDelete(T[] items){
-
+        int deletedWordsCounter=0;
+        for (int i=0;i<items.length;i++){
+            boolean deleted=this.delete(items[i]);
+            if (deleted){
+                deletedWordsCounter++;
+            }
+            else{
+                System.out.println("The Word '"+items[i]+"' Does Not Exist.");
+            }
+        }
+        System.out.println("Number of Deleted Words = "+deletedWordsCounter+" From "+items.length+" Words Received.");
     }
 
+    protected void reHashing(int length,T[] insertedElements){
+        boolean collisionsExist = true;
+        int numberOfInsertedElements=0;
+        ArrayList<String> messages = new ArrayList<String>();
+        counterOfRehashing = 0;
+        T[] temp;
+        while (collisionsExist){
+            temp = (T[]) new Object[length*length];
+            for (int i = 0; i < length*length; i++){temp[i] = null;}
+            allocated = 0;
+            counterOfRehashing++;
+            messages.clear();
+            collisionsExist = false;
+            generateHashFunction();
+            for (int i = 0; i < this.size; i++){
+                int index=getHashIndex(this.primaryTable[i]);
+                if (temp[index]==null){
+                    temp[index]=this.primaryTable[i];
+                    allocated++;
+                    numberOfInsertedElements++;
+                }else if (temp[index].equals(this.primaryTable[i])) {
+                    messages.add("The Word '" + temp[i] + "' Already Exists.");
+                }else{
+                    collisionsExist=true;
+                    break;
+                }
+            }
+            if (!collisionsExist){
+                for (int i = 0; i < insertedElements.length; i++){
+                    int index=getHashIndex(insertedElements[i]);
+                    if (temp[index]==null){
+                        temp[index]=insertedElements[i];
+                        allocated++;
+                        numberOfInsertedElements++;
+                    }else if (temp[index].equals(insertedElements[i])) {
+                        messages.add("The Word '" + temp[i] + "' Already Exists.");
+                    }else{
+                        collisionsExist=true;
+                        break;
+                    }
+                }
+            }
+            if (!collisionsExist){
+                for (int i = 0; i < messages.size(); i++){
+                    System.out.println(messages.get(i));
+                }
+                System.out.println("As Rehashing was Necessary, it Took"+counterOfRehashing+" times to Rebuild the Hash Table.");
+                if(numberOfInsertedElements>1) System.out.println("Number of Inserted Elements = "+numberOfInsertedElements+" From "+insertedElements.length+" Received Words.");
+            }
+        }
+    }
 
 }

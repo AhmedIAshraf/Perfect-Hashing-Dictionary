b8d9320df87f4cf42643fbf3b36d603295635705
HagerMelook <121068101+HagerMelook@users.noreply.github.com>
2024-04-20 17:45:55 +0200

    Add files via upload

---
+++ Dictionary/src/main/java/org/dictionary/Dictionary.java
@@ -0,0 +1,71 @@
+package org.dictionary;
+
+public class Dictionary <T> {
+    private final int method; // if 1 --> linear, 2 --> quadratic
+    private HashTable<Integer> hashTable;
+
+    public Dictionary(int method) {
+        this.method = method;
+        if (this.method == 1)
+            hashTable = new LinearSpace<>();
+        else
+            hashTable = new QuadraticSpace<>();
+    }
+    /**
+     * Inserts a word into the dictionary.
+     *
+     * @param word The word to be inserted.
+     * @return {@code true} if the word was successfully inserted, {@code false} if it already exists.
+     */
+    public boolean insert(String word) {
+        boolean test=hashTable.insert(word.hashCode());
+        return test;
+    }
+
+    /**
+     * Deletes a word from the dictionary.
+     *
+     * @param word The word to be deleted.
+     * @return {@code true} if the word was successfully deleted, {@code false} if it doesn't
+     * exist in the dictionary.
+     */
+    public boolean delete(String word) {
+        boolean test=hashTable.delete(word.hashCode());
+        return test;
+    }
+
+    /**
+     * Searches for a word in the dictionary.
+     *
+     * @param word The word to be searched.
+     * @return {@code true} if the word is found in the dictionary, {@code false} if it doesn't
+     * exist in the dictionary.
+     */
+    public boolean search(String word) {
+        boolean test=hashTable.search(word.hashCode());
+        return test;
+    }
+
+    /**
+     * Inserts an array of words read from file into the dictionary.
+     *
+     * @param words The array of words to be inserted.
+     */
+    public void batchInsert(String[] words) {
+        Integer [] word_hashcode=new Integer[words.length];
+        for(int i =0;i<words.length;i++) word_hashcode[i]=words[i].hashCode();
+        hashTable.batchInsert(word_hashcode);
+    }
+
+    /**
+     * Deletes an array of words read from file from the dictionary.
+     *
+     * @param words The array of words to be deleted.
+     */
+    public void batchDelete(String[] words) {
+        Integer [] word_hashcode=new Integer[words.length];
+        for(int i =0;i<words.length;i++) word_hashcode[i]=words[i].hashCode();
+        hashTable.batchDelete(word_hashcode);
+    }
+}
+
---
+++ Dictionary/src/main/java/org/dictionary/LinearSpace.java
@@ -0,0 +1,37 @@
+package org.dictionary;
+
+public class LinearSpace<T> extends HashTable<T>{
+
+    @Override
+    boolean insert(T value) {
+        //System.out.println("hello");
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    boolean delete(T value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    boolean search(T value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    void batchInsert(T[] items) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'batchInsert'");
+    }
+
+    @Override
+    void batchDelete(T[] items) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'batchDelete'");
+    }
+    
+    
+}
